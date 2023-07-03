0717de224712b504d8b58329275a18c6c80e4cf8
DebasishMohanta <debasish2018rta@gmail.com>
2023-07-03 11:01:38 +0530

    v2

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = ["weather/static",]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ["weather/static"]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
