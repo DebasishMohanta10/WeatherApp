7df53c2030e5089cee5a0bb8506d3313b31f5cb5
DebasishMohanta <debasish2018rta@gmail.com>
2023-09-02 09:52:26 +0530

    latest changes

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ["weather/static"]
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
